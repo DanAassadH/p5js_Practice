faa9f9389f42a4c19be777ee6fbc061fdf026872
Danish <danishriz100@gmail.comgit commitexit>
2020-12-18 16:26:46 -0700

    slider with rotateing ellipse

--- Slider.js
+++ Slider.js
@@ -1,28 +1,22 @@
-let rSlider, gSlider, bSlider;
+
+let detailY;
 
 function setup() {
-  // create canvas
-  createCanvas(710, 400);
-  textSize(15);
-  noStroke();
 
-  // create sliders
-  rSlider = createSlider(0, 255, 100);
-  rSlider.position(20, 20);
-  gSlider = createSlider(0, 255, 0);
-  gSlider.position(20, 50);
-  bSlider = createSlider(0, 255, 255);
-  bSlider.position(20, 80);
+
+  let cnv = createCanvas(100, 100, WEBGL);
+  cnv.position(200, 200);
+
+  detailY = createSlider(2, 24, 6);
+  detailY.position(10, height + 5);
+  detailY.style('width', '80px');
 }
 
 function draw() {
-  const r = rSlider.value();
-  const g = gSlider.value();
-  const b = bSlider.value();
-  background(r, g, b);
-  text('red', rSlider.x * 2 + rSlider.width, 35);
-  text('green', gSlider.x * 2 + gSlider.width, 65);
-  text('blue', bSlider.x * 2 + bSlider.width, 95);
+  background(205, 105, 94);
+
+  rotateY(millis() / 1000);
+  ellipsoid(30, 40, 40, 12, detailY.value());
 }
 
 
